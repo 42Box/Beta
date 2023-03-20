d8c289ee63320dc877dce3fb6dd0b0bc4b15ebb4
JoowonPark <ghonman2@ajou.ac.kr>
2023-03-20 11:30:35 +0900

    GOGO

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  BrowserRouter
 } from "react-router-dom"
 import Home from './routes/Home'
 import Box from './routes/Box'
 import Token from './Token'
 
+
 function App() {
-  return <Router>
+  return 
+    <BrowserRouter base = {process.env.PUBLIC_URL}>
     <Routes>
       <Route path={process.env.PUBLIC_URL + "/"} element={<Home />}/>
       <Route path={process.env.PUBLIC_URL +"/home"}element={<Token />}/>
       <Route path={process.env.PUBLIC_URL + "/box" }element={<Box />}/>
     </Routes>
-  </Router>
+  </BrowserRouter>
 }
 
 export default App;
